3dd4ff850b04caa482d93f0361df4dd616ae1be1
Vinpapounet <jack.gaarkeuken@gmail.com>
2020-08-07 15:47:32 -0400

    next app

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Link from 'next/link'
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import Link from "next/link";
 
-export default () => (
-  <div>
-    <h1>Index page</h1>
+export default ({ content }) => (
+  <div sx={{ height: `calc(100vh - 60px)` }}>
+    <div
+      sx={{
+        variant: "containers.page",
+        display: "flex",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
+    </div>
+  </div>
+);
 
-    <Link href="/notes">
-      <a>Notes</a>
-    </Link>
-  </div> 
-)
+export async function getStaticProps() {
+  return {
+    props: {
+      content: {
+        title: "Look at my note app tho",
+      },
+    },
+  };
+}
--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-export default () => {
-  const router = useRouter()
-  const { id }= router.query
+const Note = () => {
+  const router = useRouter();
+  const { id } = router.query;
 
   return (
-    <div>
+    <div sx={{ variant: "containers.page" }}>
       <h1>Note: {id} </h1>
-
-      <Link href="/notes">
-        <a>Notes</a>
-      </Link>
     </div>
-  )
+  );
+};
+
+export default Note;
+
+export async function getServerSideProps({ params, req, res }) {
+  const response = await fetch(`http://localhost:3000/api/note/${params.id}`);
+
+  if (!response.ok) {
+    res.writeHead(302, { Location: "/notes" });
+    res.end();
+    return { props: {} };
+  }
+
+  const { data } = await response.json();
+
+  if (data) {
+    return {
+      props: { note: data },
+    };
+  }
 }
